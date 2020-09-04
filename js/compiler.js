6fc99ef98fea3b8a73bac228c7e8944f0c0733bf
unknown <1414907794@qq.com>
2020-09-04 18:37:59 +0800

    vue

---
+++ js/compiler.js
@@ -0,0 +1,94 @@
+class Compiler{
+    constructor(vm){
+        this.el = vm.$el;
+        this.vm = vm;
+        this.compile(this.el);
+    }
+    // 编译模板,处理文本节点和元素节点
+    compile(el){
+        let childNodes = el.childNodes; //伪数组
+        Array.from(childNodes).forEach(node => {
+            if(this.isTextNode(node)){
+                // 处理文本节点
+                this.compileText(node);
+            }else if(this.isElementNode(node)){
+                // 处理元素节点
+                this.compileElement(node);
+            }
+
+            // 判断node节点是否有子节点,如果有,递归调用compile
+            if(node.childNodes && node.childNodes.length){
+                this.compile(node);
+            }
+        })
+    }
+    // 编译元素节点,处理指令
+    compileElement(node){
+        // 获取属性节点
+        // 遍历所有属性节点，判断是否是指令
+        Array.from(node.attributes).forEach(attr => {
+            let attrName = attr.name;
+            if(this.isDirective(attrName)){
+                //v-text --> text
+                attrName = attrName.substr(2);
+                let key = attr.value;
+                this.update(node,key,attrName);
+            }
+        })
+    }
+    update(node,key,attrName){
+        let updateFn = this[attrName + 'Updater'];
+        updateFn && updateFn.call(this,node,this.vm[key],key);
+    }
+    // 处理v-text指令
+    textUpdater(node,value,key){
+        node.textContent = value;
+        new Watcher(this.vm,key,(newValue) => {
+            node.textContent = newValue;
+        })
+    }
+    //v-model
+    modelUpdater(node,value,key){
+        node.value = value;
+        new Watcher(this.vm,key,(newValue) => {
+            node.value = newValue;
+        })
+
+        // 双向绑定
+        node.addEventListener('input',() => {
+            this.vm[key] = node.value;
+        })
+    }
+
+
+
+
+    // 编译文本节点,处理差值表达式
+    compileText(node){
+        // console.dir(node);//变量以对象形式打印
+        // {{ 变量名 }}
+        let reg = /\{\{(.+?)\}\}/;
+        let value = node.textContent;
+        if(reg.test(value)){
+            let key = RegExp.$1.trim();
+            node.textContent = value.replace(reg,this.vm[key]);
+
+            // 创建watcher对象，当数据改变更新视图
+            new Watcher(this.vm,key,(newValue) => {
+                node.textContent = newValue
+            })
+        }
+    }
+    // 判断元素属性是否是指令
+    isDirective(attrName){
+        return attrName.startsWith('v-');
+    }
+    // 判断节点是否是文本节点
+    isTextNode(node){
+        return node.nodeType === 3;
+    }
+    // 判断是否是元素节点
+    isElementNode(node){
+        return node.nodeType === 1;
+    }
+}
